92cc66c32c31689bb9fc1b3879040af5e8b481be
Marcos Castelo <marcoscastelo@Marcoss-MacBook-Air.local>
2022-06-12 20:00:15 -0300

    odissey lift off part 1

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+const GET_TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      author {
+        photo
+        name
+        id
+      }
+      modulesCount
+      length
+      thumbnail
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { data, loading, error  } = useQuery(GET_TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map(track => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
